15f8c2a3f62fbf47e2283970b76b3c63a53cf7db
DataScienceSalon <datasciencesalon@gmail.com>
2018-04-03 00:50:24 -0400

    Initial commit

---
+++ man-roxygen/nlpStudio0Classes.R
@@ -0,0 +1,4 @@
+#' @section Studio0 Family of Classes Overview:
+#' The Studio0 family of classes include the TextStudio the DataStudio,
+#' and the InfoStudio classes, each of which, automate and encapsulate
+#' the processing of text, data, and information or analysis classes.
